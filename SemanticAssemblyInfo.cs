57f4221b4e8ab80c40ed006ab5e85cf2bbef2f93
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-23 10:24:09 -0800

    updated build scripts for app services

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -2,6 +2,8 @@ using System.Reflection;
 using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.1235072338")]
-[assembly: AssemblyCommit("3ca52cf")]
-[assembly: AssemblySemanticVersion("1.0.5-test+3ca52cf")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyCommit("8c5e0bc")]
+[assembly: AssemblySemanticVersion("1.0.5-test+8c5e0bc")]
+[assembly: AssemblyDescription("; SemanticVersion=1.0.5-test+8c5e0bc, Revision=1.0.5.2186337872")]
+[assembly: AssemblyProduct("bam.net.data.graphql")]
